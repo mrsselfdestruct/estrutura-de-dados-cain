0cf22b85ae797215942c6712f93298ae3ccabfc6
sam 🖤 <134349748+mrsselfdestruct@users.noreply.github.com>
2025-09-10 19:29:31 -0400

    Create aula5.js

---
+++ 03-09-2025/aula5.js
@@ -0,0 +1,11 @@
+let name = "Uninorte";
+let text = "missing";
+let result = name ?? text
+
+let x = 6;
+let y = -3;
+let z = (x > 0 && y > 0)
+
+console.log("O valor é: " + result);
+
+console.log("O valor de Z é: " + z);
