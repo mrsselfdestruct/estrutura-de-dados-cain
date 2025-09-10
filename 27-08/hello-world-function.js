5f229f7f6fd525be0921a4d5b3b8a63a792963d0
sam 🖤 <134349748+mrsselfdestruct@users.noreply.github.com>
2025-09-10 19:26:24 -0400

    Update and rename hello-world.js to hello-world-function.js

--- 27-08/hello-world-function.js
+++ 27-08/hello-world-function.js
@@ -1,3 +1,10 @@
+// node - - version: Checar versão do Node.js, caso esteja instalado
+// npm init (-y): Commando para iniciar um projeto Node.js.
+// Ctrl + C: Parar a execução do Node.
+// cd.. : Voltar atrás uma pasta.
+// dir: Ver a pasta e os arquivos dentro da pasta.
+// node (arquivo).js: Executa o arquivo.
+
 console.log("Iniciando projeto")
 const { createServer } = require('node:http'); // biblioteca do node.
 const fs = require('fs');
@@ -6,10 +13,8 @@ const data = fs.readFileSync('myfile.txt', 'utf8'); // escaneia arquivo criado d
 console.log('2. File contents:', data); // data é o que está dentro do arquivo criado (citado acima).
 console.log('3. Done reading file');
 
-
 const hostname = '127.0.0.1'; // variável para definir endereço.
 const port = 3000; // porta da aplicação.
-
 
 const server = createServer((req, res) => { // req -> Requisição, res -> Resposta
   res.statusCode = 200;
@@ -17,7 +22,17 @@ const server = createServer((req, res) => { // req -> Requisição, res -> Respo
   res.end('Hello World');
 });
 
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+---------------------------------------------------------------------------------
+  
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+console.log("O valor é:", myFunction(2,3))
+// Cria uma função para calcular a divisão e imprimir no console.
+
+// *: Multiplicação | / : Divisão
