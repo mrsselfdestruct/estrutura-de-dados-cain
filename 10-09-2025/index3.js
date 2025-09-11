667265632818bea3356fc9fbfef993de1bbb0bc5
sam 🖤 <134349748+mrsselfdestruct@users.noreply.github.com>
2025-09-10 21:30:34 -0400

    Create index3.js

---
+++ 10-09-2025/index3.js
@@ -0,0 +1,12 @@
+function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+  this.nationality = "English";
+}
+
+const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
+
+console.log(myFather.firstName)
